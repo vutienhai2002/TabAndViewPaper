d9beefec97987b6caf6466a0cdcfdc584e007e41
vutienhai2002 <vutienhai2002@gmail.com>
2024-01-30 22:26:56 +0700

    TabAndViewPaper

---
+++ app/src/main/java/com/example/tabandviewpaper/ViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.tabandviewpaper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import android.os.Bundle;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    private static final int CARD_ITEM_SIZE = 3;
+    static final int TAB_HOME = 1;
+    static final int TAB_ABOUT = 2;
+    static final int TAB_SETTING = 3;
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+    @NonNull @Override public Fragment createFragment(int position) {
+        Fragment fragment = new HomeFragment();
+        switch (position){
+            case TAB_HOME:
+                fragment = new HomeFragment();
+                break;
+            case TAB_ABOUT:
+                fragment = new AboutFragment();
+                break;
+            case TAB_SETTING:
+                fragment = new SettingFragment();
+                break;
+        }
+        return fragment;
+    }
+    @Override public int getItemCount() {
+        return CARD_ITEM_SIZE;
+    }
+}
